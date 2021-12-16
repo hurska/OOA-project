730816ca018f2286dd7a995b1a4728a66acca03d
dkuzm <dkuzm@softserveinc.com>
2021-12-16 14:11:09 +0200

    Add resource folder with sql files

---
+++ src/main/resources/USERSVISASINSERT.sql
@@ -0,0 +1,160 @@
+INSERT INTO USERS_VISAS (id_user,id_visa)
+               VALUES (40,13);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (19,6);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (15,1);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (29,8);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (44,18);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (58,4);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (11,22);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (37,17);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (22,15);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (61,18);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (45,4);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (48,21);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (24,2);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (59,18);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (14,22);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (26,9);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (46,2);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (24,19);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (31,10);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (43,24);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (20,15);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (62,1);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (48,22);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (15,13);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (41,13);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (14,12);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (9,25);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (20,3);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (42,5);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (19,9);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (51,13);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (58,13);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (20,22);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (7,15);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (61,3);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (21,3);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (15,21);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (58,9);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (24,8);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (41,3);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (32,16);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (5,21);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (46,15);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (44,19);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (6,1);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (5,24);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (15,20);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (61,15);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (23,21);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (48,2);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (41,17);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (10,24);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (3,2);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (1,15);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (32,23);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (47,13);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (4,5);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (28,8);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (55,20);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (16,19);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (50,11);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (19,5);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (43,1);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (24,22);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (24,1);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (3,18);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (15,7);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (58,18);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (59,12);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (35,11);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (34,5);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (7,23);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (61,11);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (31,18);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (4,25);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (49,7);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (34,5);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (24,13);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (1,7);
+INSERT INTO USERS_VISAS (id_user,id_visa)
+VALUES (31,24);
